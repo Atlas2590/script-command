41dc1ba8b7e848494c5498d5bddec378021cedeb
Atlas2590 <137615883+Atlas2590@users.noreply.github.com>
2025-07-08 13:41:02 +0200

    Update create-task-send-backup-log.ps1

--- create-task-send-backup-log.ps1
+++ create-task-send-backup-log.ps1
@@ -2,10 +2,12 @@
 $scriptPath = "irm https://raw.githubusercontent.com/Atlas2590/script-command/refs/heads/main/sendBackupLog.ps1 | iex"
 $taskName = "BackupLogPolling"
 
-# === CREA LA TASK CHE ESEGUE LO SCRIPT OGNI 5 MINUTI ===
+# === CREA LA TASK CHE ESEGUE LO SCRIPT OGNI 3 VOLTE AL GIORNO ===
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -Command `"$scriptPath`""
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration (New-TimeSpan -Hours 23)
+$trigger1 = New-ScheduledTaskTrigger -Daily -At 23pm
+$trigger2 = New-ScheduledTaskTrigger -Daily -At 3am
+$trigger3 = New-ScheduledTaskTrigger -Daily -At 8am
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Principal $principal -Force
-Write-Host "✅ Task '$taskName' creata. Verifica che lo script sia in: $scriptPath"
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger @($trigger1, $trigger2, $trigger3) -Principal $principal -Force
+Write-Host "✅ Task '$taskName' creata. Verifica che lo script sia in: Task Scheduler"
