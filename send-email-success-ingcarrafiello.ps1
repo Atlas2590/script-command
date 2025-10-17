a7ed3b36b08ac2956dc08970ba4e73196e25024c
Atlas2590 <137615883+Atlas2590@users.noreply.github.com>
2025-10-17 10:07:12 +0200

    email backup personalizzata
    This script retrieves organizational unit information and sends a success email after a backup operation.

---
+++ send-email-success-ingcarrafiello.ps1
@@ -0,0 +1,18 @@
+
+$OU = Get-ADOrganizationalUnit -Filter 'Name -notlike "Domain Controllers"' -SearchScope OneLevel
+$NomeOU = $OU.Name
+# RECUPERA IL NOME DELLA SCUOLA DALL'UNITA' ORGANIZZATIVA PADRE
+
+#RECUPERA LE CREDENZIALI DI ACCESSO
+Import-Module CredentialManager
+
+$cred = Get-StoredCredential -Target "mail"
+
+# INVIA EMAIL
+$From = "report@sistema54.com"
+$To = "nanni.carrafiello@gmail.com"
+$Subject = "SERVER $NomeOU Success Backup"
+$Body = "Backup completo con Windows Server Backup effettuato con successo."
+$Password = $cred.Password
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "report@sistema54.com", $Password
+Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" -port 587 -UseSsl -Credential $Credential
